c9e49ba870f3c97f47bb4ba9099f555269f14c73
Anthony <anthonykmbanks@gmail.com>
2021-04-22 14:03:39 -0400

    commit tree 3df8ca79c08fa1086c85279ee311f83d5b33b3f1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    state = {
+        yaba: []
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    yaba: data.daba
+                })
+            })
+    }
+}
+
+
+
+export default App
